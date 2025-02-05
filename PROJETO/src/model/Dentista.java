87a4856d1e7f04798b686ba64b989b44adc13c01
eduardasanttana <125045346+EduardaSanttana@users.noreply.github.com>
2025-02-05 14:55:03 -0300

    Update Dentista.java

--- PROJETO/src/model/Dentista.java
+++ PROJETO/src/model/Dentista.java
@@ -1,22 +1,25 @@
 package model;
 
 public class Dentista extends Funcionario {
-
     private double cro;
+    private double comissao; 
 
     public Dentista(String nome, String telefone, String dataNascimento, Endereco endereco, double salario, double cro) {
         super(nome, telefone, dataNascimento, endereco, salario);
         this.cro = cro;
+        this.comissao = 0;
     }
 
     public double getCro() {
         return cro;
     }
 
+    public void adicionarComissao(double valorConsulta) {
+        this.comissao += valorConsulta * 0.1; 
+    }
+
     @Override
     public double calculoSalario() {
-        double comissaoTotal = super.getSalario() * 0.1;
-        
-        return super.getSalario() + comissaoTotal;
+        return super.getSalario() + comissao; 
     }
 }
